573262816acee854c5b7fc7a34ff733d35c8e670
Patryk <krasowski.p@outlook.com>
2023-12-01 21:20:43 +0100

    add Header and Quiz component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
-function App() {}
+import Header from './components/Header.jsx'
+import Quiz from './components/Quiz.jsx'
 
-export default App;
+const App = () => {
+	return (
+		<>
+			<Header></Header>
+			<main>
+				<Quiz />
+			</main>
+		</>
+	)
+}
+
+export default App
