7bc9c0467c4925e330822760612380580874983e
Patryk <krasowski.p@outlook.com>
2023-12-05 20:54:00 +0100

    created Summary component responsible for display summary quiz

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,8 @@
 import { useCallback, useState } from 'react'
 
 import QUESTIONS from '../question.js'
-import quizCompleteImg from '../assets/quiz-complete.png'
 import Question from './Question.jsx'
+import Summary from './Summary.jsx'
 
 const Quiz = () => {
 	const [userAnswers, setUserAnswers] = useState([])
@@ -19,12 +19,7 @@ const Quiz = () => {
 	const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer])
 
 	if (quizIsComplete) {
-		return (
-			<div id='summary'>
-				<img src={quizCompleteImg} alt='Trophy icon' />
-				<h2>Quiz Completed!</h2>
-			</div>
-		)
+		return <Summary userAnswers={userAnswers} />
 	}
 
 	return (
